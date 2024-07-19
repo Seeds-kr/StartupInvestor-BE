bb03ccb68728088dfbb8124a46fac331c008ae13
jiseop <ghdwltjq5749@naver.com>
2024-07-19 23:11:16 +0900

    [STAR-12][Feat] : SpringBoot 의존성 적용 후 프로젝트 생성

---
+++ src/main/java/seeds/StartupInvestor/StartupInvestorApplication.java
@@ -0,0 +1,13 @@
+package seeds.StartupInvestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupInvestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartupInvestorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seeds/StartupInvestor/StartupInvestorApplicationTests.java
@@ -0,0 +1,13 @@
+package seeds.StartupInvestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupInvestorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
